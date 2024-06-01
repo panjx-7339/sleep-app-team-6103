1097f2c78fe0abc4bd5e64f97df2674733afc97f
maertan <119797875+maertan@users.noreply.github.com>
2024-06-01 15:45:08 +0800

    Updated CurrentDate

---
+++ src/screens/HomeScreen/components/CurrentDate.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const CurrentDate = () => {
+  return <Text className="font-bold text-2xl">Saturday 1 Jun 2024</Text>;
+};
+
+export default CurrentDate;
+
+const styles = StyleSheet.create({});
--- src/screens/HomeScreen/components/TopBar.js
+++ src/screens/HomeScreen/components/TopBar.js
@@ -1,15 +1,16 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import SignOutButton from "./SignOutButton";
-import Date from "./Date";
+import Date from "./CurrentDate";
 import SleepGoalDisplay from "./SleepGoalDisplay";
 import PointsDisplay from "./PointsDisplay";
+import CurrentDate from "./CurrentDate";
 
 const TopBar = () => {
   return (
     <View className="flex-row h-24">
       <View className="w-2/3 flex-col justify-center">
-        <Date />
+        <CurrentDate />
         <SleepGoalDisplay />
       </View>
       <View className="w-1/3 flex-col justify-center items-center">
