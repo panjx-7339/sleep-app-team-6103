88eb4bee91bee2f084605d42f0008d8a24e267e7
maertan <119797875+maertan@users.noreply.github.com>
2024-06-01 15:14:32 +0800

    Updated SleepGoalDisplay

--- src/screens/HomeScreen/components/SleepGoalDisplay.js
+++ src/screens/HomeScreen/components/SleepGoalDisplay.js
@@ -1,8 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
 
 const SleepGoalDisplay = () => {
-  return <Text>Today's Goal: 8 Hours</Text>;
+  return (
+    <TouchableOpacity className=" bg-blue-800 mt-1 border-2 border-gray-500 px-1 py-1 rounded-lg w-2/3">
+      <Text className="text-center text-white">Today's Goal: 8 Hour</Text>
+    </TouchableOpacity>
+  );
 };
 
 export default SleepGoalDisplay;
