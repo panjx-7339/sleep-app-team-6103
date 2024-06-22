74b5b9b9051ec99a031cd55ce4edf9c2c121e671
panjx-7339 <panjinxia06@gmail.com>
2024-06-22 18:13:50 +0800

    added cancel button to sleepGoalDisplay

--- src/screens/HomeScreen/components/SleepGoalDisplay.js
+++ src/screens/HomeScreen/components/SleepGoalDisplay.js
@@ -5,6 +5,8 @@ import {
   TouchableOpacity,
   TextInput,
   Modal,
+  KeyboardAvoidingView,
+  Platform,
 } from "react-native";
 import { React, useState, useEffect } from "react";
 import { auth, db } from "../../../firebase/config";
@@ -60,8 +62,6 @@ const SleepGoalDisplay = () => {
       } finally {
         setModalVisible(!modalVisible);
       }
-    } else {
-      alert("Error", "User is not logged in");
     }
   };
 
@@ -72,11 +72,10 @@ const SleepGoalDisplay = () => {
         transparent={true}
         visible={modalVisible}
         onRequestClose={() => {
-          console.log("Modal has been closed.");
           setModalVisible(!modalVisible);
         }}
       >
-        <View style={styles.centeredView}>
+        <KeyboardAvoidingView style={styles.centeredView} behavior={Platform.OS === "android" ? "height" : "padding"}>
           <View style={styles.modalContainer}>
             <Text style={[styles.text, {color:"#fff"}]}>
               Please enter your sleep goal, in hours:
@@ -87,14 +86,23 @@ const SleepGoalDisplay = () => {
               value={input}
               keyboardType="numeric"
             />
-            <TouchableOpacity
-              style={styles.button}
-              onPress={addSleepGoal}
-            >
-              <Text style={styles.text}>Submit</Text>
-            </TouchableOpacity>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+              style={[styles.button,  {backgroundColor: "lightcoral"}]}
+              onPress={() => setModalVisible(!modalVisible)}
+              >
+                <Text style={styles.text}>Cancel</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={[styles.button, {backgroundColor: "palegreen"}]}
+                onPress={addSleepGoal}
+              >
+                <Text style={styles.text}>Submit</Text>
+              </TouchableOpacity>
+            </View>
           </View>
-        </View>
+        </KeyboardAvoidingView>
       </Modal>
 
       <TouchableOpacity
@@ -113,36 +121,40 @@ const styles = StyleSheet.create({
   centeredView: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
+    alignItems: "center"
   },
   modalContainer: {
     backgroundColor: "#6C6CB3",
-    padding: 20,
     borderColor: "#fff",
     borderWidth: 3,
     borderRadius: 30,
     alignItems: "center",
-    width: "80%"
+    justifyContent: "center",
+    width: "80%", 
+    // height: "20%"
   },
   inputContainer: {
     backgroundColor: "#fff",
     fontFamily: "K2D",
     borderRadius: 20,
-    paddingHorizontal: 15,
     marginVertical: 20,
-    width: "90%",
-    textAlign: "center",
+    width: "80%",
+    height: "15%",
+    textAlign: "center"
   },
   text: {
     fontFamily: "K2DBold",
     fontSize: 15,
     color: "#000",
-    textAlign: "center",
+    textAlign: "center"
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "center"
   },
   button: {
-    backgroundColor: "#fff",
-    borderRadius: 30,
-    marginVertical: 5,
-    width: "22%",
+    borderRadius: 20,
+    marginHorizontal: 10,
+    padding: "1%"
   }
 });
